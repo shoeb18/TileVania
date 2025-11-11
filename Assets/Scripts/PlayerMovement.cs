b1d2cdc89597ae684ff7884dafdf5da39134d8b8
shoeb khan <shoebkhan0197@gmail.com>
2025-11-11 08:57:17 +0530

    Add climbing mechanics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,16 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
     [SerializeField] float jumpForce = 10f;
-    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float climbSpeed = 10f;
 
     Vector2 moveInput;
     Rigidbody2D rb;
     Animator animator;
     BoxCollider2D boxCollider;
 
-    private bool playerHasMoveInput;
+    private bool playerHasHorizontalVelocity;
+    private bool playerHasVerticalVelocity;
+    private float defaultGravityScale;
 
     void Awake()
     {
@@ -22,24 +24,47 @@ public class PlayerMovement : MonoBehaviour
         boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    void Start()
+    {
+        defaultGravityScale = rb.gravityScale;
+    }
+
     void Update()
     {
         Movement();
-        Flip();
+        FlipSprite();
+        ClimbLadder();
     }
 
     void Movement()
     {
         rb.linearVelocity = new Vector2(moveInput.x * movementSpeed, rb.linearVelocityY);
-        animator.SetBool("IsRunning", playerHasMoveInput);
+        animator.SetBool("IsRunning", playerHasHorizontalVelocity);
     }
 
-    void Flip()
+    void FlipSprite()
     {
-        playerHasMoveInput = Mathf.Abs(rb.linearVelocityX) > Mathf.Epsilon;
-        if (playerHasMoveInput)
+        playerHasHorizontalVelocity = Mathf.Abs(rb.linearVelocityX) > Mathf.Epsilon;
+        if (playerHasHorizontalVelocity)
         {
             transform.localScale = new Vector2(Mathf.Sign(rb.linearVelocityX), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if (boxCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, moveInput.y * climbSpeed);
+            rb.gravityScale = 0;
+
+            playerHasVerticalVelocity = Mathf.Abs(rb.linearVelocityY) > Mathf.Epsilon;
+            animator.SetBool("IsClimbing", playerHasVerticalVelocity);
+        }
+        else
+        {
+            rb.gravityScale = defaultGravityScale;
+            animator.SetBool("IsClimbing", false);
         }
     }
     
@@ -50,7 +75,7 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if (value.isPressed && boxCollider.IsTouchingLayers(groundLayer))
+        if (value.isPressed && boxCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
             rb.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
